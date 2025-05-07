771cd4cfd144cbfa09418fff7f1884d1c138d8f8
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2025-05-08 01:36:28 +0300

    add migrations for refresh token

---
+++ Modules/Identity/Identity.Infrastructure/Persistence/Migrations/20250506221011_AddRefreshTokens.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                schema: "identity",
+                columns: table => new
+                {
+                    IdentityId = table.Column<int>(type: "integer", nullable: false),
+                    AccessToken = table.Column<string>(type: "text", nullable: false),
+                    ProjectToken = table.Column<string>(type: "text", nullable: true),
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    ExpiryDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.IdentityId);
+                    table.ForeignKey(
+                        name: "FK_RefreshTokens_Identities_IdentityId",
+                        column: x => x.IdentityId,
+                        principalSchema: "identity",
+                        principalTable: "Identities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens",
+                schema: "identity");
+        }
+    }
+}
