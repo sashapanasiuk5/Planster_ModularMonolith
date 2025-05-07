5985ff213d725c0df35df2f4eed332d4a83447ab
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2025-05-08 01:36:28 +0300

    add migrations for refresh token

--- Modules/Identity/Identity.Infrastructure/Persistence/IdentityDbContext.cs
+++ Modules/Identity/Identity.Infrastructure/Persistence/IdentityDbContext.cs
@@ -1,10 +1,13 @@
+using Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence;
 
+using Identity = Domain.Models.Identity;
 public class IdentityDbContext: DbContext
 {
-    public DbSet<Domain.Models.Identity> Identities { get; set; }
+    public DbSet<Identity> Identities { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
     
     public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options) { }
 
@@ -21,18 +24,26 @@ public class IdentityDbContext: DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("identity");
-        modelBuilder.Entity<Domain.Models.Identity>()
+        modelBuilder.Entity<Identity>()
             .HasKey(x => x.Id);
 
-        modelBuilder.Entity<Domain.Models.Identity>()
+        modelBuilder.Entity<Identity>()
             .Property(p => p.Id)
             .ValueGeneratedNever();
 
-        modelBuilder.Entity<Domain.Models.Identity>()
+        modelBuilder.Entity<Identity>()
             .OwnsOne(x => x.Credentials, credentials =>
             {
                 credentials.Property(x => x.Password).HasColumnName("Password");
                 credentials.Property(x => x.Email).HasColumnName("Email");
             });
+        
+        modelBuilder.Entity<RefreshToken>()
+            .HasKey(p => p.IdentityId);
+
+        modelBuilder.Entity<RefreshToken>()
+            .HasOne(p => p.Identity)
+            .WithOne(u => u.RefreshToken)
+            .HasForeignKey<RefreshToken>(p => p.IdentityId);
     }
 }
